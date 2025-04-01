ab7c8e244bdc1c6a84709234e170f6d4b9488409
f.alimoradi <fatemehalimoradi1965@gmail.com>
2025-04-01 17:31:01 +0330

    database sketch

---
+++ src/db/Database.java
@@ -0,0 +1,45 @@
+package db;
+
+import db.exception.EntityNotFoundException;
+import java.util.ArrayList;
+
+public class Database {
+    private static ArrayList<Entity> entities = new ArrayList<>();
+    private static int entityCount = 0;
+
+    public static void add(Entity e) {
+        entityCount++;
+        e.id = entityCount;
+        entities.add(e);
+    }
+    public static Entity get(int id) throws EntityNotFoundException {
+        for (int i = 0; i < entities.size(); i++) {
+            if (entities.get(i).id == id) {
+                return entities.get(i);
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+    public static void delete(int id) throws EntityNotFoundException {
+        boolean notExist = true;
+        for (int i = 0; i < entities.size(); i++) {
+            if (entities.get(i).id == id) {
+                entities.remove(i);
+                notExist = false;
+            }
+        }
+        if (notExist) {
+            throw new EntityNotFoundException(id);
+        }
+    }
+    public static void update(Entity e) throws EntityNotFoundException {
+        if (entities.contains(e)) {
+            entities.add(entities.indexOf(e),e);
+        }
+        else {
+            throw new EntityNotFoundException();
+        }
+    }
+}
+
+
